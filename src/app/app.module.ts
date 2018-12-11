a6e3c9c9220accd6c8e073cd710283440e4b611e
Mike Ludemann <mike-ludemann@hotmail.de>
2018-12-11 17:08:01 +0100

    Simple updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-/* Components */
-import { AppComponent } from './app.component';
-import { ImageComponent } from './components/image/image.component';
-
 /* Services */
 import { CalculateService } from './services/calculate.service';
 
 /* Pipes (Module) */
 import { PipesModule } from './pipes/pipes.module';
 
+/* Components */
+import { AppComponent } from './app.component';
+import { ImageComponent } from './components/image/image.component';
+import { BoldComponent } from './components/tags/bold/bold.component';
+import { ItalicComponent } from './components/tags/italic/italic.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ImageComponent
+    ImageComponent,
+    BoldComponent,
+    ItalicComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-image',
+  selector: 'image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css']
 })
 export class ImageComponent implements OnInit {
-  //@Input() source: string;
+
+  @Input() id: string;
+  @Input() class: string;
+  @Input() source: string;
+  @Input() alttext: string;
+  @Input() width: string;
+  @Input() height: string;
 
   constructor() { }
 
